668c81daa0206e37cf4a098600bd0dbf3a2eee2f
mcasta13 <116083045+mcasta13@users.noreply.github.com>
2022-10-26 09:25:14 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
+let suma = 0
 let n1 = 0
 let n2 = 0
-let suma = 0
-function nombres () {
+input.onButtonPressed(Button.A, function () {
+    nombresatzar()
+})
+function suma1 () {
+    suma = n1 + n2
+    basic.showNumber(suma)
+}
+function nombresatzar () {
     n1 = randint(0, 10)
     n2 = randint(0, 10)
     basic.showNumber(n1)
     basic.showString("+")
     basic.showNumber(n2)
 }
-input.onButtonPressed(Button.A, function () {
-    nombres()
-})
 input.onButtonPressed(Button.B, function () {
-    suma2()
+    suma1()
 })
-function suma2 () {
-    suma = n1 + n2
-    basic.showNumber(suma)
-}
 basic.forever(function () {
 	
 })
